26c8187dc5a7664a37fa614f45bf73826dd840b9
Cycl0matic <muhammadalaidy@protonmail.com>
2020-11-05 21:46:24 +0200

    add Test.java and static imports

---
+++ src/cyphervm/Test.java
@@ -0,0 +1,21 @@
+package cyphervm;
+
+import static cyphervm.Bytecode.BR;
+import static cyphervm.Bytecode.BRF;
+import static cyphervm.Bytecode.CALL;
+import static cyphervm.Bytecode.GLOAD;
+import static cyphervm.Bytecode.GSTORE;
+import static cyphervm.Bytecode.HALT;
+import static cyphervm.Bytecode.IADD;
+import static cyphervm.Bytecode.ICONST;
+import static cyphervm.Bytecode.ILT;
+import static cyphervm.Bytecode.IMUL;
+import static cyphervm.Bytecode.ISUB;
+import static cyphervm.Bytecode.LOAD;
+import static cyphervm.Bytecode.PRINT;
+import static cyphervm.Bytecode.RET;
+import static cyphervm.Bytecode.STORE;
+
+public class Test {
+
+}
