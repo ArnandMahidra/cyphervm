7675b4480b35120b41f5f1d09253c5917ec589fb
Cycl0matic <muhammadalaidy@protonmail.com>
2020-11-06 05:15:23 +0200

    finish test.java

--- src/cyphervm/Test.java
+++ src/cyphervm/Test.java
@@ -22,8 +22,28 @@ public class Test {
     static int[] loop = { ICONST, 10, GSTORE, 0, ICONST, 0, GSTORE, 1, GLOAD, 1, GLOAD, 0, ILT, BRF, 24, GLOAD, 1,
             ICONST, 1, IADD, GSTORE, 1, BR, 8, HALT };
 
-
     static FuncMetaData[] loop_metadata = { new FuncMetaData("main", 0, 0, 0) };
 
+    static int FACTORIAL_INDEX = 1;
+    static int FACTORIAL_ADDRESS = 0;
+    static int MAIN_ADDRESS = 21;
+
+    static int[] factorial = { LOAD, 0, ICONST, 2, ILT, BRF, 10, ICONST, 1, RET, LOAD, 0, LOAD, 0, ICONST, 1, ISUB,
+            CALL, FACTORIAL_INDEX, IMUL, RET, ICONST, 5, CALL, FACTORIAL_INDEX, PRINT, HALT };
+
+    static FuncMetaData[] factorial_metadata = {
+            // .def factorial: ARGS=1, LOCALS=0 ADDRESS
+            new FuncMetaData("main", 0, 0, MAIN_ADDRESS), new FuncMetaData("factorial", 1, 0, FACTORIAL_ADDRESS) };
+
+    static int[] f = { ICONST, 10, CALL, 1, PRINT, HALT,
+            // .def f(x): ARGS=1, LOCALS=1
+            // a = x;
+            LOAD, 0, // 6 <-- start of f
+            STORE, 1,
+            // return 2*a
+            LOAD, 1, ICONST, 2, IMUL, RET };
+
+    static FuncMetaData[] f_metadata = { new FuncMetaData("main", 0, 0, 0), new FuncMetaData("f", 1, 1, 6) };
     
+
 }
