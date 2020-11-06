3dea4ac5d679da725ad847a40832f85c25a992df
Cycl0matic <muhammadalaidy@protonmail.com>
2020-11-06 05:09:54 +0200

    test.java modified and add FuncMetaData

--- src/cyphervm/Test.java
+++ src/cyphervm/Test.java
@@ -17,5 +17,13 @@ import static cyphervm.Bytecode.RET;
 import static cyphervm.Bytecode.STORE;
 
 public class Test {
+    static int[] hello = { ICONST, 1, ICONST, 2, IADD, PRINT, HALT };
 
+    static int[] loop = { ICONST, 10, GSTORE, 0, ICONST, 0, GSTORE, 1, GLOAD, 1, GLOAD, 0, ILT, BRF, 24, GLOAD, 1,
+            ICONST, 1, IADD, GSTORE, 1, BR, 8, HALT };
+
+
+    static FuncMetaData[] loop_metadata = { new FuncMetaData("main", 0, 0, 0) };
+
+    
 }
